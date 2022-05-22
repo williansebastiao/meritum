2d2898103107eacccc7c6cf1ceb2572105a48f34
williansebastiao <willians@4vconnect.com>
2022-05-21 21:55:43 -0300

    feature: added methods to controllers

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,38 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\AuthRequest;
+use App\Http\Requests\Api\RegisterRequest;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    private $user;
+
+    public function __construct()
+    {
+
+    }
+
+    public function register(RegisterRequest $request): object
+    {
+
+    }
+
+    public function authenticate(AuthRequest $request): object
+    {
+
+    }
+
+    public function me(): object
+    {
+
+    }
+
+    public function logout(): object
+    {
+
+    }
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'register'], function(){
+    Route::post('/', [\App\Http\Controllers\Api\UserController::class, 'authenticate']);
+    Route::group(['middleware' => 'jwt.verify'], function() {
+        Route::get('me', [\App\Http\Controllers\Api\UserController::class, 'me']);
+        Route::post('logout', [\App\Http\Controllers\Api\UserController::class, 'logout']);
+    });
 });
